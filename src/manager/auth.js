233c1fc280c7401b496f62e6d48544d1fd4b3a45
Nawan <z5527177@ad.edu.unsw.au>
2026-01-31 13:18:48 +0800

    added eslint and fixed linting

--- src/manager/auth.js
+++ src/manager/auth.js
@@ -1,156 +1,156 @@
-import { prisma } from "../config/db.js";
-import bcrypt from "bcryptjs";
-import { generateToken } from "../utils/generateToken.js";
-import validator from "validator";
+import { prisma } from '../config/db.js'
+import bcrypt from 'bcryptjs'
+import { generateToken } from '../utils/generateToken.js'
+import validator from 'validator'
 
 async function register(name, email, password, res) {
 
-    if (!validator.isEmail(email)) {
-        throw new Error("The email provided is not a valid email");
-    }
+	if (!validator.isEmail(email)) {
+		throw new Error('The email provided is not a valid email')
+	}
 
-    if (!validator.isAlpha(name, 'en-US', { ignore: '_- ' })) {
-        throw new Error('Error: Name contains invalid characters');
-    }
+	if (!validator.isAlpha(name, 'en-US', { ignore: '_- ' })) {
+		throw new Error('Error: Name contains invalid characters')
+	}
 
-    if (name.length < 2 || name.length > 50) {
-        throw new Error('Error: Name is either too large or too small');
-    }
+	if (name.length < 2 || name.length > 50) {
+		throw new Error('Error: Name is either too large or too small')
+	}
 
-    if (password.length < 6) {
-        throw new Error('Error: password is too small');
-    }
+	if (password.length < 6) {
+		throw new Error('Error: password is too small')
+	}
 
-    const isValid = /^(?=.*[A-Za-z])(?=.*\d).+$/.test(password);
+	const isValid = /^(?=.*[A-Za-z])(?=.*\d).+$/.test(password)
 
-    if (!isValid) {
-        throw new Error("The password entred must contain at least one letter and one number");
-    }
+	if (!isValid) {
+		throw new Error('The password entred must contain at least one letter and one number')
+	}
 
-    const userExists = await prisma.user.findUnique({
-        where: {email: email },
-    })
+	const userExists = await prisma.user.findUnique({
+		where: { email: email },
+	})
 
-    if (userExists) {
-        throw new Error("User already exists with this email");
-    }
+	if (userExists) {
+		throw new Error('User already exists with this email')
+	}
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPasswrod = await bcrypt.hash(password, salt);
+	const salt = await bcrypt.genSalt(10)
+	const hashedPasswrod = await bcrypt.hash(password, salt)
 
-    const user = await prisma.user.create({
-        data: {
-            name, 
-            email,
-            password: hashedPasswrod,
-        }
-    });
-    
-    generateToken(user.id, email, res);
+	const user = await prisma.user.create({
+		data: {
+			name,
+			email,
+			password: hashedPasswrod,
+		},
+	})
 
-    const data = {
-        status: "success",
-        data: {
-            user: {
-                id: user.id,
-                name: name,
-                email: email,
-            },
-        }
-    }
+	generateToken(user.id, email, res)
 
-    return data
+	const data = {
+		status: 'success',
+		data: {
+			user: {
+				id: user.id,
+				name: name,
+				email: email,
+			},
+		},
+	}
+
+	return data
 }
 
 async function login(email, password, res) {
-    const user = await prisma.user.findUnique({
-        where: { email: email },
-    })
+	const user = await prisma.user.findUnique({
+		where: { email: email },
+	})
 
-    if (!user) {
-        throw new Error("Invalid email or password");
-    }
+	if (!user) {
+		throw new Error('Invalid email or password')
+	}
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+	const isPasswordValid = await bcrypt.compare(password, user.password)
 
-    if (!isPasswordValid) {
-        throw new Error("Invalid email or password");
-    }
-    
-    generateToken(user.id, email, res);
+	if (!isPasswordValid) {
+		throw new Error('Invalid email or password')
+	}
 
-    const data = {
-        status: "success",
-        data: {
-            user: {
-                id: user.id,
-                name: user.name,
-                email: email,
-            },
-        }
-    };
+	generateToken(user.id, email, res)
 
-    return data
+	const data = {
+		status: 'success',
+		data: {
+			user: {
+				id: user.id,
+				name: user.name,
+				email: email,
+			},
+		},
+	}
+
+	return data
 }
 
 async function deleteUser(user) {
-    if (!user) {
-        throw new Error("Unauthorized");
-    }
+	if (!user) {
+		throw new Error('Unauthorized')
+	}
 
-    await prisma.user.delete({
-        where: {id: user.id}
-    });
+	await prisma.user.delete({
+		where: { id: user.id },
+	})
 
-    const data = {
-        status: "success",
-        message: "User deleted succesfully"
-    };
+	const data = {
+		status: 'success',
+		message: 'User deleted succesfully',
+	}
 
-    return data
+	return data
 }
 
 async function updateUserDetails(user, email, password, name) {
-    if (!user) {
-        throw new Error("Unauthorized");
-    }
+	if (!user) {
+		throw new Error('Unauthorized')
+	}
 
-    const currentUser = await prisma.user.findUnique({
-        where: { id: user.id },
-    });
+	const currentUser = await prisma.user.findUnique({
+		where: { id: user.id },
+	})
 
-    if (!currentUser) {
-        throw new Error("User not found");
-    }
+	if (!currentUser) {
+		throw new Error('User not found')
+	}
 
-    if (email !== undefined && email !== null && email !== currentUser.email) {
-        const checkEmail = await prisma.user.findUnique({
-            where: { email },
-        });
+	if (email !== undefined && email !== null && email !== currentUser.email) {
+		const checkEmail = await prisma.user.findUnique({
+			where: { email },
+		})
 
-        if (checkEmail) {
-            throw new Error("User already exists with this email");
-        }
-    }
+		if (checkEmail) {
+			throw new Error('User already exists with this email')
+		}
+	}
 
-    const updateData = {
-        email: email ?? currentUser.email,
-        password: password ?? currentUser.password,
-        name: name ?? currentUser.name,
-        updatedAt: new Date()
-    };
+	const updateData = {
+		email: email ?? currentUser.email,
+		password: password ?? currentUser.password,
+		name: name ?? currentUser.name,
+		updatedAt: new Date(),
+	}
 
-    const updatedUser = await prisma.user.update({
-        where: { id: user.id },
-        data: updateData,
-    });
+	const updatedUser = await prisma.user.update({
+		where: { id: user.id },
+		data: updateData,
+	})
 
-    return {
-        status: "success",
-        data: {
-            user: updatedUser,
-        },
-    };
+	return {
+		status: 'success',
+		data: {
+			user: updatedUser,
+		},
+	}
 }
 
 export { register, login, deleteUser, updateUserDetails }
--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,20 +1,20 @@
 import jwt from 'jsonwebtoken'
-import crypto from 'crypto';
+import crypto from 'crypto'
 
 export function generateToken(userId, email, res) {
-    const payload = {jti: crypto.randomUUID(), id: userId, email};
-    const token = jwt.sign(payload, process.env.JWT, {
-        expiresIn: process.env.JWT_EXPIRES_IN || "7d",
-    });
+	const payload = { jti: crypto.randomUUID(), id: userId, email }
+	const token = jwt.sign(payload, process.env.JWT, {
+		expiresIn: process.env.JWT_EXPIRES_IN || '7d',
+	})
 
-    console.log(token)
+	console.log(token)
 
-    res.cookie("jwt", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        path: '/',
-        maxAge: (1000 * 60 * 60 * 24) * 7
-    });
-    return token;
+	res.cookie('jwt', token, {
+		httpOnly: true,
+		secure: process.env.NODE_ENV === 'production',
+		sameSite: 'strict',
+		path: '/',
+		maxAge: (1000 * 60 * 60 * 24) * 7,
+	})
+	return token
 }
